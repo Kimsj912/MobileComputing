540408013f717c716fe5d98767ec198a7d067df5
sujk912 <aa991122aa@gmail.com>
2022-05-02 13:22:45 +0900

    week9-practice-Check and set Preference

--- app/src/main/java/com/example/mobilecomputing/MainActivity.kt
+++ app/src/main/java/com/example/mobilecomputing/MainActivity.kt
@@ -1,17 +1,8 @@
 package com.example.mobilecomputing
 
-import android.content.Context
 import android.os.Bundle
-import android.util.Log
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
-import androidx.core.content.edit
 import com.example.mobilecomputing.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -22,24 +13,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        supportFragmentManager.beginTransaction().replace(binding.setting.id, MySettingsFragment()).commit()
 
-        val sharedPref = getPreferences(Context.MODE_PRIVATE)
-        val sharedPref2 = getSharedPreferences("pref_test1", Context.MODE_PRIVATE)
-
-        sharedPref.edit(){
-            putBoolean("hi", true)
-            putString("Store", "Emart everyday")
-            putInt("Number", 10)
-            apply()
-        }
-        sharedPref2.edit(){
-            putBoolean("hi", false)
-            putString("Store", "Daiso everyday")
-            putInt("Number", 5)
-            apply()
-        }
-
-        sharedPref.getString("Store","?")?.let{Log.d("Debug", it)}
-        sharedPref2.getString("Store","?")?.let{Log.d("Debug", it)}
     }
 }
---
+++ app/src/main/java/com/example/mobilecomputing/MySettingsFragment.kt
@@ -0,0 +1,12 @@
+package com.example.mobilecomputing
+
+import android.os.Bundle
+import androidx.preference.PreferenceFragmentCompat
+
+class MySettingsFragment : PreferenceFragmentCompat() {
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.root_preferences, rootKey)
+
+    }
+}
