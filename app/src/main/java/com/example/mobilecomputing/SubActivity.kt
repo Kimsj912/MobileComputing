cd21b1a284f3e0cc2e785811f553dffa0ea203b1
sujk912 <aa991122aa@gmail.com>
2022-05-11 12:30:03 +0900

    Week10-1:  startActivity

---
+++ app/src/main/java/com/example/mobilecomputing/SubActivity.kt
@@ -0,0 +1,11 @@
+package com.example.mobilecomputing
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SubActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.sub_activity)
+    }
+}
