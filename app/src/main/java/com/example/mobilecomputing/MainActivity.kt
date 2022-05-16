40f6473b423b9e0747f34dc564ff8cb266af6784
sujk912 <aa991122aa@gmail.com>
2022-05-16 12:58:43 +0900

    Week10-2: LifeCycle test

--- app/src/main/java/com/example/mobilecomputing/MainActivity.kt
+++ app/src/main/java/com/example/mobilecomputing/MainActivity.kt
@@ -6,6 +6,7 @@ import android.content.Intent
 import android.graphics.*
 import android.net.Uri
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.util.Log
 import android.view.View
 import android.widget.Toast
@@ -18,95 +19,56 @@ import com.example.mobilecomputing.databinding.ActivityMainAlterBinding
 import com.example.mobilecomputing.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    var count = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d("MYTAG", "onCreate $count")
         var binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val reqLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
-            result:ActivityResult ->
-                if(result.resultCode == RESULT_OK){
-                    Log.d("TAG", "return")
-                    val intent = result.data
-                    val clickedBtn :String? = intent?.getStringExtra("res")
-                    "btn : $clickedBtn".also{ binding.txtMainView1.text = it}
-                }
-        }
-
-
-
-        binding.btnSub.setOnClickListener {
-            val intent:Intent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:114")).apply {
-            putExtra("next", "level")
-            }
-            intent.putExtra("num",30)
-            intent.putExtra("edit",binding.editText.text.toString())
-
-            try{
-//                reqLauncher.launch(intent)
-                startActivity(intent)
-            } catch (e:ActivityNotFoundException) {
-                Toast.makeText(applicationContext, "No app for action", Toast.LENGTH_LONG).show()
-            }
-
+        "Count: $count".also{binding.counterTxt.text = it}
+        binding.counterBtn.setOnClickListener {
+            count++
+            Log.d("MYTAG", "Btn clicked $count")
+            "Count: $count".also{binding.counterTxt.text = it}
         }
     }
-}
 
-//
-//class MyCustomView(context: Context): View(context){
-//    override fun onDraw(canvas: Canvas) {
-//        super.onDraw(canvas)
-//        val paint = Paint()
-//        paint.isAntiAlias = true
-//        paint.color = Color.GRAY
-//        canvas.drawLine(20f, 20f, 600f, 20f, paint)
-//
-//        paint.strokeWidth = 5f
-//        paint.style = Paint.Style.FILL
-//        val rect1 = Rect(30,130,30+100, 130+100)
-//        canvas.drawRect(rect1, paint)
-//
-//        paint.style = Paint.Style.STROKE
-//        val rect2 = Rect(140,130,140+100, 130+100)
-//        canvas.drawRect(rect2, paint)
-//
-//        paint.style = Paint.Style.FILL_AND_STROKE
-//        val rect3 = Rect(300,130,300+100, 130+100)
-//        canvas.drawRect(rect3, paint)
-//
-//        paint.style = Paint.Style.STROKE
-//        val rect4 = RectF(420f,130f,420f+100f, 130f+100f)
-//        canvas.drawRoundRect(rect4, 10f, 10f, paint)
-//
-//        val rect5 = RectF(550f,130f,550f+100f, 130f+100f)
-//        canvas.drawRoundRect(rect5, 50f, 50f, paint)
-//        canvas.drawCircle(680f, 130f, 50f, paint)
-//        canvas.drawPoint(680f, 130f, paint)
-//
-//        path1.moveTo(20f,300f)
-//        path1.lineTo(20f+100f, 300f+100f)
-//        path1.lineTo(20f+200f, 300f)
-//        path1.lineTo(20f+300f, 300f+100f)
-//        path1.lineTo(20f+400f, 300f)
-//        canvas.drawPath(path1, paint)
-//
-//        var len = 100f
-//        var tx = 20f
-//        var ty = 600f
-//        path2.moveTo(tx,ty)
-//        while(len>10f){
-//            for(i in (1..4)){
-//                tx+=dx[i]*len
-//                ty+=dy[i]*len
-//                path2.lineTo(tx,ty)
-//                if(i%2==0) len*=0.9f
-//            }
-//        }
-//        canvas.drawPath(path2, paint)
-//    }
-//    private val path1: Path = Path()
-//    private val path2: Path = Path()
-//    private val dx = arrayOf(0,1,0,-1,0)
-//    private val dy = arrayOf(0,0,1,0,-1)
-//}
+    override fun onStart() {
+        super.onStart()
+        Log.d("MYTAG", "onStart $count")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("MYTAG", "onResume $count")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("MYTAG", "onPause $count")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("MYTAG", "onStop $count")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("MYTAG", "onDestroy $count")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        Log.d("MYTAG", "onSaveInstance $count")
+        outState.putInt("count", count)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        Log.d("MYTAG", "onRestoreInstance $count")
+        count = savedInstanceState.getInt("count", 0)
+        Log.d("MYTAG", "onRestoreInstanceLoad $count")
+    }
+}
